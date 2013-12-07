3d1e600e11937149e1c43058643abe3774d9699e
TakumiBaba <s09704tb@gmail.com>
2013-12-07 19:19:38 +0900

    native

---
+++ babascript-client/BabaScriptClient/src/main/java/org/babascript/android/Client.java
@@ -0,0 +1,41 @@
+package org.babascript.android;
+
+import org.json.JSONObject;
+
+/**
+ * Created by takumi on 2013/12/07.
+ */
+public class Client implements Callback {
+
+    Client(){
+
+    }
+
+    public void returnValue(){
+
+    }
+    @Override
+    public void connect(JSONObject json) {
+
+    }
+
+    @Override
+    public void write(JSONObject json) {
+
+    }
+
+    @Override
+    public void read(JSONObject json) {
+
+    }
+
+    @Override
+    public void take(JSONObject json) {
+
+    }
+
+    @Override
+    public void watch(JSONObject json) {
+
+    }
+}
